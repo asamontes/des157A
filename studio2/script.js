968d516cf49801eca29a5101900d74abaf478b17
April Montes <asamontes@ucdavis.edu>
2024-02-29 11:48:25 -0800

    updated

--- studio2/script.js
+++ studio2/script.js
@@ -1,9 +1,4 @@
 (function(){
     'use strict';
     console.log('reading JS');
-
-    document.querySelector('pickme').addEventListener('click', function(event){
-        event.preventDefault();
-        document.querySelector('#overlay1').className = "show";
-    });
-})()
+})();
